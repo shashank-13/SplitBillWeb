e743956741d86c623c1e49060255bce748d52629
Shashank Singh <shashanksingh2797@gmail.com>
2017-04-25 11:10:08 +0530

    Algorithm done

--- Controller/home.js
+++ Controller/home.js
@@ -179,20 +179,23 @@ deletedata:function(req,res)
 
 insertManual:function(req,res)
 {
-    var registrationToken='fXLBSeWgMYo:APA91bEx6EnuDhqH6jYJbYIhJQ1VujI_g2EtgEmTohqAuPErCnDxiNLNSZazAayhgb14-G0A_GRt2gXeFIokjenGDxBPHoeVt-7_2UQ3KAHOhkyCtJIm_hW4umDV6F3ZEJ6ecCgrTogD';
-    var notification='APA91bHybYGoTH00ZMGdZQBS5qqOhPsI0c5g83nle03TvL4SOxv_6bsP6prY3Pnil9LyaTrje8xXPfSWq_UFetkcnAwyCDDRpnPQQSN7wTPSCp76LQQVlGA';
-    var newRow = new centraldb({notificationKey:notification,valueAmount:40,token:
-     	registrationToken});
-     	  newRow.save(function(err,record)
+  var group1,group2,key1,key2;
+	group1='mygroup';
+	group2='mahisbathan';
+	key1='APA91bF3vYv61Mtys2jJMb0gw_2UeEzh65wrogzYexpw6lijR1ec6Li5BXvop6RoLuKVJ8s5j_Gi2Ub6JMGz6JSlLWuQTJwzjxGqC-lQ_nw0w_CGuBQ-gsc';
+	key2='APA91bGsWEEJwXR6OcXrV7as-KsLSOlRDHsBj6gUK-eq5qyydnj6voqjEuRLZqCu5NSk81hqBWR3NuiB4EEZ2vRHSElPiVlGXd1lI5Eq4ho0_vTRvHoZ7yI';
+	var token1='ccUcMskEJYg:APA91bH-_cDWHJNjutrY7_nvh8Ra8kp-41z_-ml6IAXDUCmHLvQ8TbOEk5AG9nt4r9Q26fPXMjKUj0B3FFx1pKi43nLrwMlwxPqcbAsR6ykI6G6ZriJooU8uKngNoKbgpBdk83qbVohl';
+	var token2='fXLBSeWgMYo:APA91bEx6EnuDhqH6jYJbYIhJQ1VujI_g2EtgEmTohqAuPErCnDxiNLNSZazAayhgb14-G0A_GRt2gXeFIokjenGDxBPHoeVt-7_2UQ3KAHOhkyCtJIm_hW4umDV6F3ZEJ6ecCgrTogD';
+
+	centraldb.update({notificationKey:key1},{$set:{valueAmount:0}},{upsert:true,multi:true},function()
 	{
-	if(err)
-	throw err;
-	console.log('Row added Successfully');
-	console.log(record.notificationKey);
-	console.log(record.valueAmount);
-	console.log(record.token);
-	  res.status(201).json({message : "Token updated succesfully"});
-	});    
+		console.log('Central database updated');
+	});
+
+	centraldb.update({notificationKey:key2},{$set:{valueAmount:0}},{upsert:true,multi:true},function()
+	{
+		console.log('Central database updated');
+	});
 }
 
 
--- Controller/updatedb.js
+++ Controller/updatedb.js
@@ -1,70 +1,96 @@
 var querydb = require('../database/centraldb.js');
 var admin = require('firebase-admin');
+var token=require('../database/token.js');
+var notify=require('../database/notification.js');
+var asyncLoop = require('node-async-loop');
+
 
 module.exports = {
-    index: function(req, res) {
-        if (req.body.key && req.body.amount && req.body.message) {
-            var token_val = req.headers['x-access-token'];
 
-            querydb.update({
-                notificationKey: req.body.key,
-                token: token_val
-            }, {
-                $set: {
-                    notificationKey: req.body.key,
-                    token: token_val,
-                    valueAmount: req.body.amount
-                }
-            }, {
-                upsert: true,
-                multi: true
-            }, function() {
-                querydb.findOne({
-                    notificationKey: req.body.key,
-                    token: token_val
-                }, function(err, user) {
 
-                    if (err)
-                        throw err;
-                    if (user) {
-                        console.log(user);
-                        console.log(user.valueAmount);
-                        console.log(req.body.amount);
-                        if (user.valueAmount == req.body.amount) {
-                            console.log('Should send correct value')
-													var payLoad={notification:{title:"Group Expense",body:req.body.message}};
-													admin.messaging().sendToDevice(req.body.key,payLoad).then(function(response)
+    index:function(req,res)
+    {
+        var data=req.body;
+        var notification=req.headers['key'];
+        var message=req.headers['message'];
+        console.log(data);
+        console.log(notification);
+        console.log(message);
 
-													{
-														console.log("Successfully message ",response);
-													})
-													.catch(function(error)
-													{
-												     console.log('Error Sending message'+error);
-													});
-                            res.status(201).json({message: 'success'});
-                        } else {
-                            console.log('User found but data did not matched');
-                            res.json({
-                                message: 'null'
-                            });
-                        }
-                    } else {
-                        console.log('user not found');
-                        res.json({
-                            message: 'null'
-                        });
+        if(data && notification)
+        {
+            asyncLoop(data,function(item,next)
+            {
+
+            token.findOne({user:item['key']},function(err,user)
+            {
+                if(err)
+                    throw err;
+                var tokenVal = user.userToken;
+                querydb.findOne({notificationKey:notification,token:tokenVal},function(err,user)
+                {
+                    if(!err)
+                    {
+                        var prev=user.valueAmount;
+                        var new_amount = item['value']+prev;
+                        querydb.update({notificationKey:notification,token:tokenVal}, {
+                            $set: {
+                             notificationKey: notification,
+                             token: tokenVal,
+                             valueAmount: new_amount
+                            }
+                        }, {
+                             upsert: true,
+                            multi: true
+                             }, function() {
+                                next();
+                             });
+
                     }
-
+                    else
+                    {
+                        querydb.update({notificationKey:notification,token:tokenVal}, {
+                            $set: {
+                             notificationKey: notification,
+                             token: tokenVal,
+                             valueAmount: item['value']
+                            }
+                        }, {
+                             upsert: true,
+                            multi: true
+                             }, function() {
+                                next();
+                             });
+                    }
 
                 });
             });
+            },function(err)
+            {
+                if (err)
+                {
+                    console.error('Error: ' + err.message);
+                    return;
+                }
 
-        } else {
-            console.log('Error in data recieved from device');
-            res.json({
-                message: 'null'
+                var payLoad={notification:{title:"Group Expense",body:message}};
+                admin.messaging().sendToDevice(notification,payLoad).then(function(response)
+
+                {
+                    console.log("Successfully message ",response);
+                })
+                .catch(function(error)
+                {
+                 console.log('Error Sending message'+error);
+                });
+            console.log('Finished!');
+            res.status(201).json( {message : 'Successfully done'});
+
             });
+        }
+        else
+        {
+            res.status(201).json({message:'null'});
         }
 
     }
