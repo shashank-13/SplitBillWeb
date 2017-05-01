4f6e00792cc3bc5d74d746938370e2e75b4bc693
Shashank Singh <shashanksingh2797@gmail.com>
2017-05-02 02:15:31 +0530

    key changes

--- data/serverkey.js
+++ data/serverkey.js
@@ -1,4 +1,4 @@
 module.exports={
-var apiKey='key=AIzaSyDWaNecbKKuLP9ndQDMMYPLLrtawaN0Fxk';
-var SENDER_ID='178770510313';
+ apikey: 'key=AIzaSyDWaNecbKKuLP9ndQDMMYPLLrtawaN0Fxk',
+SENDER_ID :'178770510313'
 };
